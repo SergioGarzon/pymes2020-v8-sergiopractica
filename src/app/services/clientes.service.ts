0026e04023e9f88c1a39c20cecae2d38e4e6959e
SergioGarzon <sggarzonutn@gmail.com>
2020-11-30 14:47:42 -0300

    2 files modified

--- src/app/services/clientes.service.ts
+++ src/app/services/clientes.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@angular/core';
+import {
+  HttpClient,
+  HttpParams
+} from "@angular/common/http";
+import { of } from "rxjs";
+import { Clientes } from "../models/clientes";
 
 @Injectable()
 export class ClientesService {
+  resourceUrl: string;
+  
+  constructor(private httpClient: HttpClient) {
+    this.resourceUrl = "https://pav2.azurewebsites.net/api/clientes/";
+  }
 
-  constructor() { }
+  get() {
+    return this.httpClient.get(this.resourceUrl);
+  }
 
+  getById(Id: number) {
+    return this.httpClient.get(this.resourceUrl + Id);
+  }
+
+  post(obj: Pais) {
+    return this.httpClient.post(this.resourceUrl, obj);
+  }
+  
 }
